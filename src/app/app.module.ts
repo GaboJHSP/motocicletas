cd446515cfe7b5c30fe290886ffff9e35668a3d2
GaboJHSP <104600151+GaboJHSP@users.noreply.github.com>
2022-10-31 13:22:21 -0400

    Version 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MotosComponent } from './motos/motos.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { ContactosComponent } from './contactos/contactos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MotosComponent,
+    FormularioComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
